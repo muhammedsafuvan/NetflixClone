4f7c0d44992dec4e3e35cc77c40fb68e7da05b1d
Muhammed Safuvan <muhammedsafuvan024@gmail.com>
2021-10-17 13:26:44 +0530

    first commit

---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl = "https://api.themoviedb.org/3";
+export const API_KEY = "fe05e315dd4375a649d27d86207b968d";
+export const imageUrl =  'https://image.tmdb.org/t/p/original';
---
+++ src/url.js
@@ -0,0 +1,13 @@
+ import {API_KEY} from './constants/constants'
+
+export const originals = `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US`
+
+export const action = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=28`
+
+export const comedy = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=35`
+
+export const horror = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=27`
+
+export const documentery = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=99`
+
+export const romance = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=10749`
